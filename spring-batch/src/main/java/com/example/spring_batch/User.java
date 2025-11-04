42ae61fba0615c8cbbedc7f974f6bcf0e93c15b8
yogoloper <jguddnjs@gmail.com>
2025-11-04 21:39:27 +0900

    itemReader - database 실습

--- spring-batch/src/main/java/com/example/spring_batch/User.java
+++ spring-batch/src/main/java/com/example/spring_batch/User.java
@@ -1,10 +1,17 @@
 package com.example.spring_batch;
 
+import jakarta.persistence.*;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@Entity
+@Table(name = "user")
+@NoArgsConstructor
 public class User {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private String name;
 
     private String age;
