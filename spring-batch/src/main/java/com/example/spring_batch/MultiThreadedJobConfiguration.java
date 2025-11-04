03e0fb219d5b4cbd1f80c4095e14bbe535d63a45
yogoloper <jguddnjs@gmail.com>
2025-11-04 23:26:26 +0900

    parallel 방식 처리 실습

--- spring-batch/src/main/java/com/example/spring_batch/MultiThreadedJobConfiguration.java
+++ spring-batch/src/main/java/com/example/spring_batch/MultiThreadedJobConfiguration.java
@@ -18,7 +18,7 @@ import org.springframework.core.task.SimpleAsyncTaskExecutor;
 import org.springframework.transaction.PlatformTransactionManager;
 
 @Slf4j
-@Configuration
+//@Configuration
 public class MultiThreadedJobConfiguration {
 
     @Bean
