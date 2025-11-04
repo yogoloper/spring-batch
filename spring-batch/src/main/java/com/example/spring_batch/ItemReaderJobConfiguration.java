7d0a70a37bdd8fab5dd6ff158aa08e1054af51d6
yogoloper <jguddnjs@gmail.com>
2025-11-04 21:20:00 +0900

    itemReader - file 실습 2

--- spring-batch/src/main/java/com/example/spring_batch/ItemReaderJobConfiguration.java
+++ spring-batch/src/main/java/com/example/spring_batch/ItemReaderJobConfiguration.java
@@ -9,6 +9,10 @@ import org.springframework.batch.core.step.builder.StepBuilder;
 import org.springframework.batch.item.ItemReader;
 import org.springframework.batch.item.file.FlatFileItemReader;
 import org.springframework.batch.item.file.builder.FlatFileItemReaderBuilder;
+import org.springframework.batch.item.file.transform.Range;
+import org.springframework.batch.item.json.JacksonJsonObjectReader;
+import org.springframework.batch.item.json.JsonItemReader;
+import org.springframework.batch.item.json.builder.JsonItemReaderBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.ClassPathResource;
@@ -32,11 +36,11 @@ public class ItemReaderJobConfiguration {
     public Step step(
             JobRepository jobRepository,
             PlatformTransactionManager platformTransactionManager,
-            ItemReader<User> flatFileItemReader
+            ItemReader<User> jsonItemReader
     ) {
         return new StepBuilder("step", jobRepository)
                 .<User, User>chunk(2, platformTransactionManager)
-                .reader(flatFileItemReader)
+                .reader(jsonItemReader)
                 .writer(System.out::println)
                 .build();
     }
@@ -52,4 +56,28 @@ public class ItemReaderJobConfiguration {
                 .targetType(User.class)
                 .build();
     }
+
+    @Bean
+    public FlatFileItemReader<User> fixedLengthFlatFileItemReader() {
+        return new FlatFileItemReaderBuilder<User>()
+                .name("fixedLengthFlatFileItemReader")
+                .resource(new ClassPathResource("usersFixedLength.txt"))
+                .linesToSkip(2)
+                .fixedLength()
+                .columns(new Range[]{new Range(1, 2), new Range(3, 4), new Range(5, 6), new Range(7, 19)})
+                .names("name", "age", "region", "telephone")
+                .targetType(User.class)
+                .strict(false)
+                .build();
+    }
+
+    @Bean
+    public JsonItemReader<User> jsonItemReader() {
+        return new JsonItemReaderBuilder<User>()
+                .name("jsonItemReader")
+                .resource(new ClassPathResource("users.json"))
+                .jsonObjectReader(new JacksonJsonObjectReader<>(User.class))
+                .build();
+    }
+
 }
