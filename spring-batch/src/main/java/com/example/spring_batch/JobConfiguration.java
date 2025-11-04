cd26b51b969b47266c348d3726edd86aa3155d68
yogoloper <jguddnjs@gmail.com>
2025-11-04 15:45:58 +0900

    step tasklet 반복 실행

--- spring-batch/src/main/java/com/example/spring_batch/JobConfiguration.java
+++ spring-batch/src/main/java/com/example/spring_batch/JobConfiguration.java
@@ -3,9 +3,12 @@ package com.example.spring_batch;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
+import org.springframework.batch.core.StepContribution;
 import org.springframework.batch.core.job.builder.JobBuilder;
 import org.springframework.batch.core.repository.JobRepository;
+import org.springframework.batch.core.scope.context.ChunkContext;
 import org.springframework.batch.core.step.builder.StepBuilder;
+import org.springframework.batch.core.step.tasklet.Tasklet;
 import org.springframework.batch.repeat.RepeatStatus;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -24,12 +27,26 @@ public class JobConfiguration  {
 
     @Bean
     public Step step(JobRepository jobRepository, PlatformTransactionManager platformTransactionManager) {
-        return new StepBuilder("step", jobRepository)
-                .tasklet((a, b) -> {
-                    log.info("step");
-//                    throw new IllegalArgumentException("상태가 잘못 되었습니다."); // 실패 확인
+        final Tasklet tasklet = new Tasklet() {
+
+            private int count = 0;
+
+            @Override
+            public RepeatStatus execute(StepContribution a, ChunkContext b) throws Exception {
+                count++;
+
+                if (count == 15) {
+                    log.info("TaskLet FINISHED");
                     return RepeatStatus.FINISHED;
-                }, platformTransactionManager)
+                }
+
+                log.info("TaskLet CONTINUABLE {}", count);
+                return RepeatStatus.CONTINUABLE;
+            }
+        };
+
+        return new StepBuilder("step", jobRepository)
+                .tasklet(tasklet , platformTransactionManager)
                 .build();
     }
 }
