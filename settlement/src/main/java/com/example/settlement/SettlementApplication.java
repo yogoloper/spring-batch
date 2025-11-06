6923b40d37eac9d0fa683e0e35af15a2c5b66fa7
yogoloper <jguddnjs@gmail.com>
2025-11-06 20:34:04 +0900

    정산 시스템 프로젝트 init

---
+++ settlement/src/main/java/com/example/settlement/SettlementApplication.java
@@ -0,0 +1,14 @@
+package com.example.settlement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SettlementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SettlementApplication.class, args);
+		System.out.println("Hello World");
+	}
+
+}
